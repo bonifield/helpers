b44be893a7ed5b27990e85b5bd239d982ffd826d
bonifield <bonifield@users.noreply.github.com>
2021-04-06 10:44:04 -0500

    Create stream-tcp-json.py

---
+++ logstash/stream-tcp-json.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import json, socket, sys
+
+try:
+	INPUTJSONFILE = sys.argv[1]
+	HOST = str(sys.argv[2])
+	PORT = int(sys.argv[3])
+except:
+	print()
+	print("stream-tcp-json.py file.json LogstashHost LogstashPort")
+	print()
+	sys.exit(1)
+
+try:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as msg:
+	m = "[ERROR1] {}\n".format(msg)
+	sys.stderr.write(m)
+	sys.exit(1)
+
+try:
+	sock.connect((HOST, PORT))
+except socket.error as msg:
+	m = "[ERROR2] {}\n".format(msg)
+	sys.stderr.write(m)
+	sys.exit(2)
+
+with open(INPUTJSONFILE, "r") as o:
+	c = 0
+	for line in o:
+		c += 1
+		if len(line) > 2:
+			msg = line.encode('utf-8')
+			sock.send(msg)
+	print("streamed {} lines".format(c))
+o.close()
