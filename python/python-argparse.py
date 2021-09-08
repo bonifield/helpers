3716edf04af22c2dcd8f452a60b16bbe2a0affb9
bonifield <bonifield@users.noreply.github.com>
2021-09-08 12:21:07 -0500

    Update python-argparse.py

--- python/python-argparse.py
+++ python/python-argparse.py
@@ -10,12 +10,14 @@ parser = argparse.ArgumentParser(description="argparse makes it easy to assign v
 parser.add_argument("-f", "--firstname", dest="fn", default="defaultfn", type=str, help="your first name as a string")
 parser.add_argument("-l", "--lastname", dest="ln", default="defaultln", type=str, help="your last name as a string")
 parser.add_argument("-a", "--age", dest="age", default=999, type=int, help="your age as an int")
+# single-switch / no arg booleans (do not specify type=boolean with these)
+# store_true implies false (becomes true when specified on the command line); store_false implies true
+parser.add_argument("--simple", dest="simple", action="store_true", help="simplified output")
 
 # mandatory switches
 # make a new argument group then set it to required
 req = parser.add_argument_group("required arguments")
 req.add_argument("-c", "--color", dest="col", type=str, help="your favorite color", required=True)
-
 
 # treat parser as dictionary
 args = vars(parser.parse_args())
@@ -25,5 +27,9 @@ fn = args["fn"].title()
 ln = args["ln"].title()
 age = args["age"]
 col = args["col"]
+simple = args["simple"]
 
-print("Name: {} {}\nAge: {}\nFavorite Color: {}".format(fn, ln, age, col))
+if simple:
+	print(f"{fn} {ln} {age} {col}")
+else:
+	print(f"Name: {fn} {ln}\nAge: {age}\nFavorite Color: {col}")
