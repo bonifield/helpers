bffe39cd9ea0eee907cb7334a97a8b3d6cb4881a
bonifield <bonifield@users.noreply.github.com>
2022-12-14 12:37:40 -0600

    Update python-datetime-conversions.py

--- python/python-datetime-conversions.py
+++ python/python-datetime-conversions.py
@@ -1,45 +1,234 @@
 #!/usr/bin/python3
 
-from datetime import datetime
 
-#
-#
-#
 
-EPOCHEXAMPLE = 1622299682.128648 # float
-# make an epoch timestamp with datetime.now().timestamp()
+from datetime import datetime, timezone
 
-print()
-print("convert epoch to local timestamp")
-lt = datetime.fromtimestamp(EPOCHEXAMPLE).strftime("%Y-%m-%d %H:%M:%S")
-print("\t"+str(EPOCHEXAMPLE), "-->", lt)
+
+
+# start a timer
+start = datetime.utcnow().timestamp()
+
+
 
 print()
-print("convert epoch to UTC timestamp")
-ut = datetime.utcfromtimestamp(EPOCHEXAMPLE).strftime("%Y-%m-%d %H:%M:%S")
-print("\t"+str(EPOCHEXAMPLE), "-->", ut)
+print()
+print("#--------------------")
+print("# use a given epoch")
+print("#--------------------")
+print()
 
-#
-#
-#
+
+
+# generate a local epoch timestamp with datetime.now().timestamp() or UTC with datetime.utcnow().timestamp()
+EPOCHEXAMPLE = 1671033931.080355 # float
+
+print(f"EPOCHEXAMPLE = 1671033931.080355")
 
 print()
-print("local timestamps")
-print("\tdatetime.now()", "-->", datetime.now())
-print("\tdatetime.now().timestamp()", "-->", datetime.now().timestamp())
-print("\tdatetime.now().astimezone().strftime('%Y-%m-%d %H:%M:%S.%f %Z %z')", "-->", datetime.now().astimezone().strftime('%Y-%m-%d %H:%M:%S.%f %Z %z'), "(machine-local timezone info)")
-print("\tdatetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]", "-->", datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
-
-#
-#
-#
+print("convert given epoch to local timestamp: datetime.fromtimestamp(EPOCHEXAMPLE).strftime('%Y-%m-%d %H:%M:%S')")
+a = datetime.fromtimestamp(EPOCHEXAMPLE).strftime('%Y-%m-%d %H:%M:%S') # 2022-12-14 10:05:31
+print(f"\t{a}")
 
 print()
-print("UTC timestamps (note do not use astimezone() and %Z/%z here because they return the machine-local timezone info)")
-print("\tdatetime.utcnow()", "-->", datetime.utcnow())
-print("\tdatetime.utcnow().timestamp()", "-->", datetime.utcnow().timestamp())
-print("\tdatetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')", "-->", datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'))
-print("\tdatetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]", "-->", datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
-print("\tstr(datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])+'Z'", "-->", str(datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])+"Z")
+print("convert given epoch to UTC timestamp: datetime.utcfromtimestamp(EPOCHEXAMPLE).strftime('%Y-%m-%d %H:%M:%S')")
+b = datetime.utcfromtimestamp(EPOCHEXAMPLE).strftime('%Y-%m-%d %H:%M:%S') # 2022-12-14 16:05:31
+print(f"\t{b}")
+
+
 
 print()
+print()
+print("#--------------------")
+print("# local timestamps")
+print("#--------------------")
+print()
+
+
+
+print()
+print("datetime.now()")
+c = datetime.now() # 2022-12-14 10:39:48.349885
+print(f"\t{c}")
+
+print()
+print("datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]")
+d = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] # 2022-12-14 10:39:48.350
+print(f"\t{d}")
+
+print()
+print("datetime.now().timestamp()")
+e = datetime.now().timestamp() # 1671035988.349939
+print(f"\t{e}")
+
+print()
+print("datetime.now().timestamp()*1000 (milliseconds)")
+f = datetime.now().timestamp()*1000 # 1671035988349.977
+print(f"\t{f}")
+
+print()
+print("datetime.now().astimezone()")
+g = datetime.now().astimezone() # 2022-12-14 10:39:48.350018-06:00
+print(f"\t{g}")
+
+print()
+print("datetime.now().astimezone().strftime('%Y-%m-%d %H:%M:%S.%f %Z %z')")
+h = datetime.now().astimezone().strftime('%Y-%m-%d %H:%M:%S.%f %Z %z') # 2022-12-14 10:39:48.350018 CST -0600
+print(f"\t{h}")
+
+print()
+print("datetime.now().astimezone(timezone.utc)")
+i = datetime.now().astimezone(timezone.utc) # 2022-12-14 16:39:48.350018+00:00
+print(f"\t{i}")
+
+print()
+print("datetime.now().astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f %Z %z')")
+j = datetime.now().astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f %Z %z') # 2022-12-14 16:39:48.350018 UTC +0000
+print(f"\t{j}")
+
+
+
+print()
+print()
+print("#--------------------")
+print("# UTC timestamps")
+print("# note - DO NOT use astimezone() (with or without strftime and %Z/%z) here because they return the machine-local timezone info")
+print("#--------------------")
+print()
+
+
+
+print()
+print("datetime.utcnow()")
+k = datetime.utcnow() # 2022-12-14 16:39:48.350286
+print(f"\t{k}")
+
+print()
+print("datetime.utcnow().timestamp()")
+l = datetime.utcnow().timestamp() # 1671057588.350316
+print(f"\t{l}")
+
+print()
+print("datetime.utcnow().timestamp()*1000 (milliseconds)")
+m = datetime.utcnow().timestamp()*1000 # 1671057588350.361
+print(f"\t{m}")
+
+print()
+print("datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')")
+n = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f') # 2022-12-14 16:39:48.350400
+print(f"\t{n}")
+
+print()
+print("datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]")
+o = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] # 2022-12-14 16:39:48.350
+print(f"\t{o}")
+
+print()
+print("str(datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])+'Z'")
+p = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]+"Z" # 2022-12-14T16:39:48.350Z
+print(f"\t{p}")
+
+print()
+print("DO NOT DO THIS: datetime.utcnow().astimezone()")
+q = datetime.utcnow().astimezone() # VERY BAD - 2022-12-14 16:39:48.350550-06:00
+print(f"\t{q}")
+
+print()
+print("DO NOT DO THIS: datetime.utcnow().astimezone().strftime('%Y-%m-%d %H:%M:%S.%f %Z %z')")
+r = datetime.utcnow().astimezone().strftime('%Y-%m-%d %H:%M:%S.%f %Z %z') # VERY BAD - 2022-12-14 16:39:48.350604 CST -0600
+print(f"\t{r}")
+
+
+
+print()
+print()
+# end a timer
+end = datetime.utcnow().timestamp()
+complete = end-start
+print(f"script took {complete} seconds to execute")
+print()
+print()
+
+
+
+"""
+
+
+#--------------------
+# use a given epoch
+#--------------------
+
+EPOCHEXAMPLE = 1671033931.080355
+
+convert given epoch to local timestamp: datetime.fromtimestamp(EPOCHEXAMPLE).strftime('%Y-%m-%d %H:%M:%S')
+	2022-12-14 10:05:31
+
+convert given epoch to UTC timestamp: datetime.utcfromtimestamp(EPOCHEXAMPLE).strftime('%Y-%m-%d %H:%M:%S')
+	2022-12-14 16:05:31
+
+
+#--------------------
+# local timestamps
+#--------------------
+
+
+datetime.now()
+	2022-12-14 12:33:55.381492
+
+datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+	2022-12-14 12:33:55.381
+
+datetime.now().timestamp()
+	1671042835.383091
+
+datetime.now().timestamp()*1000 (milliseconds)
+	1671042835383.363
+
+datetime.now().astimezone()
+	2022-12-14 12:33:55.383625-06:00
+
+datetime.now().astimezone().strftime('%Y-%m-%d %H:%M:%S.%f %Z %z')
+	2022-12-14 12:33:55.383913 CST -0600
+
+datetime.now().astimezone(timezone.utc)
+	2022-12-14 18:33:55.384236+00:00
+
+datetime.now().astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f %Z %z')
+	2022-12-14 18:33:55.384512 UTC +0000
+
+
+#--------------------
+# UTC timestamps
+# note - DO NOT use astimezone() (with or without strftime and %Z/%z) here because they return the machine-local timezone info
+#--------------------
+
+
+datetime.utcnow()
+	2022-12-14 18:33:55.385303
+
+datetime.utcnow().timestamp()
+	1671064435.385567
+
+datetime.utcnow().timestamp()*1000 (milliseconds)
+	1671064435385.8298
+
+datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
+	2022-12-14 18:33:55.386157
+
+datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+	2022-12-14 18:33:55.386
+
+str(datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])+'Z'
+	2022-12-14T18:33:55.386Z
+
+DO NOT DO THIS: datetime.utcnow().astimezone()
+	2022-12-14 18:33:55.387008-06:00
+
+DO NOT DO THIS: datetime.utcnow().astimezone().strftime('%Y-%m-%d %H:%M:%S.%f %Z %z')
+	2022-12-14 18:33:55.387287 CST -0600
+
+
+script took 0.00855398178100586 seconds to execute
+
+
+"""
