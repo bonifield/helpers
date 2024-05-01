b7f3fb8465de42cb5c379974148137693be41bfe
Bonifield <bonifield.tools@gmail.com>
2024-04-30 22:36:10 -0500

    updated to python3

--- python/python-json-printer.py
+++ python/python-json-printer.py
@@ -1,10 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-import json,sys
+import json
+import sys
 
 try:
 	with open(sys.argv[1], 'r') as f:
 		data = json.load(f)
-		print json.dumps(data, indent=4, sort_keys="True")
+	f.close()
+	print(json.dumps(data, indent=4, sort_keys="True"))
 except Exception as e:
-	print "Usage:  json-printer.py somefile.json\n%s" % (str(e))
+	print(str(e))
+	print()
+	print("USAGE:")
+	print(f"{sys.argv[0]} somefile.json")
+	print()
