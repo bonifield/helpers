ba5bd5a83cb46601ae9cb5f4652a0e8b0e690473
Bonifield <bonifield.tools@gmail.com>
2025-08-22 22:54:18 -0500

    fixed bad templates

--- python/templates/python-template-file-open-argparse.py
+++ python/templates/python-template-file-open-argparse.py
@@ -1,53 +1,49 @@
 #!/usr/bin/env python3
 
+
 import argparse
-#import json
+import json
 import time
 
-# instantiate parser
-parser = argparse.ArgumentParser(description="script description")
 
-# optional switches
-# short arg, long arg, variable name to be used (accessed as dict), a default value (optional), variable type, help message when using -h
-parser.add_argument("-f", "--file", dest="file", default="test_file.txt", type=str, help="input file")
-parser.add_argument("-i", "--input", dest="input", default="test_in.txt", type=str, help="input filename")
-parser.add_argument("-o", "--output", dest="output", default="test_out.txt", type=str, help="output filename")
-parser.add_argument("-u", "--url", dest="url", default="google.com", type=str, help="target url")
-#parser.add_argument("-a", "--number", dest="number", default=999, type=int, help="some int")
-# single-switch / no arg booleans (do not specify type=boolean with these)
-# store_true implies false (becomes true when specified on the command line); store_false implies true
-#parser.add_argument("--simple", dest="simple", action="store_true", help="some boolean")
+def get_arguments():
+	"""Retrieves argparse values."""
+	# instantiate parser
+	parser = argparse.ArgumentParser(description="script description")
 
-# mandatory switches
-# make a new argument group then set it to required
-#req = parser.add_argument_group("required arguments")
-#req.add_argument("-m", "--mandatory", dest="mand", type=str, help="some mandatory argument", required=True)
+	# optional switches
+	parser.add_argument("-i", "--infile", dest="input_filename", default="test_in.txt", type=str, help="input filename")
+	parser.add_argument("-o", "--outfile", dest="output_filename", default="test_out.txt", type=str, help="output filename")
+	parser.add_argument("-a", "--number", dest="number", default=999, type=int, help="some number value")
+	parser.add_argument("--simple", dest="simple", action="store_true", help="some boolean value that is False unless specified, then it becomes True")
 
-# treat parser as dictionary
-args = vars(parser.parse_args())
+	# mandatory switches - make a new argument group then set it to required
+	#req = parser.add_argument_group("required arguments")
+	#req.add_argument("-m", "--mandatory", dest="mand", type=str, help="some mandatory argument", required=True)
 
-# create variables from the args object
-input_file_single = args["file"]
-input_file = args["input"]
-output_file = args["output"]
-url = args["url"]
+	return parser.parse_args()
 
 
 def process_input_file(i):
-	""" comment """
+	"""Load the input file's contents."""
 	with open(i, "r") as in_file:
 		pass
-		#j = json.load(in_file) # j is now dict of the file content
-		#pass
+		#j = json.load(in_file)
+		# or
 		#for line in in_file:
-			#l = json.loads(line) # l is now a dict
-			#pass
-	in_file.close()
+			#l = json.loads(line)
+
+
+def main():
+	"""Main execution comments go here."""
+	pass
 
 
 if __name__ == '__main__':
-	starttime = time.time()
-	process_input_file(input_file)
-	endtime = time.time()
-	fintime = str(int(endtime-starttime))
-	print(f"script finished in {fintime} seconds")
+	start = time.time()
+	# get args
+	args = get_arguments()
+	main()
+	end = time.time()
+	print(f"script finished in {end - start} seconds")
+
--- python/templates/python-template-file-open.py
+++ python/templates/python-template-file-open.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-#import json
+import json
 import sys
 import time
+
 
 try:
 	input_file = sys.argv[1]
@@ -10,21 +11,26 @@ except Exception as e:
 	print(f"Usage: {sys.argv[0]} input_file")
 	sys.exit(1)
 
+
 def process_input_file(i):
-	""" comment """
+	"""Load the input file's contents."""
 	with open(i, "r") as in_file:
 		pass
-		#j = json.load(in_file) # j is now dict of the file content
-		#pass
+		#j = json.load(in_file)
+		# or
 		#for line in in_file:
-			#l = json.loads(line) # l is now a dict
-			#pass
-	in_file.close()
+			#l = json.loads(line)
+
+
+def main():
+	"""Main execution comments go here."""
+	pass
 
 
 if __name__ == '__main__':
-	starttime = time.time()
-	process_input_file(input_file)
-	endtime = time.time()
-	fintime = str(int(endtime-starttime))
-	print(f"script finished in {fintime} seconds")
+	start = time.time()
+	# get args
+	args = get_arguments()
+	main()
+	end = time.time()
+	print(f"script finished in {end - start} seconds")
