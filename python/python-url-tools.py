6c06798a7407af66983622dde84ac15572a97c58
Bonifield <bonifield.tools@gmail.com>
2025-08-30 23:36:16 -0500

    added hostname and punycode function

--- python/python-url-tools.py
+++ python/python-url-tools.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-#\\\\\\\\\\\\\\\\\\\\
+
+#===========================
 # https://docs.python.org/3/library/html.html
 # https://docs.python.org/3/library/urllib.parse.html
-#////////////////////
+#===========================
+
 
 import os
 from html import escape
 from html import unescape
-#from urllib import parse
 from urllib.parse import quote
 from urllib.parse import unquote
 from urllib.parse import quote_plus
@@ -18,16 +19,20 @@ from urllib.parse import parse_qs
 from urllib.parse import parse_qsl
 from urllib.parse import urlparse
 
+
 #=============
 # interesting strings
 #=============
 
+
 s = "'<script>alert(1);</script>"
 e = "%27%3Cscript%3Ealert%281%29%3B%3C%2Fscript%3E"
+
 
 #=============
 # quote / unquote / quote_plus / unquote_plus
 #=============
+
 
 q = quote(s)
 print("quote\n\t", q) # %27%3Cscript%3Ealert%281%29%3B%3C/script%3E
@@ -44,16 +49,20 @@ print("quote_plus\n\t", q) # %27%3Cscript%3Ealert%281%29%3B%3C%2Fscript%3E
 q = unquote_plus(q)
 print("unquote_plus\n\t", q) # '<script>alert(1);</script>
 
+
 #=============
 # escape / unescape
 #=============
 
+
 print("escape\n\t", escape(s, quote=True)) # &lt;script&gt;alert(1);&lt;/script&gt;
 print("unescape\n\t", unescape(e)) # %3Cscript%3Ealert%281%29%3B%3C%2Fscript%3E
+
 
 #=============
 # urlencode / url "decode" (qs/qsl)
 #=============
+
 
 vars = {"key1":"val1", "key2":"val2"}
 n = urlencode(vars)
@@ -69,17 +78,22 @@ ddd = parse_qsl(n) # [('key1', 'val1'), ('key2', 'val2')]
 # qsl is reversible, can urlencode() directly from this object
 print("qsl\n\t", ddd)
 
+
 #=============
 # urlparse
 #=============
 
-u = urlparse("https://www.google.com/search?client=firefox-b-1-d&q=reddit&%3Cscript%3Ealert%281%29%3B%3C%2Fscript%3E")
+
+u = urlparse("https://myusername:mypassword@www.mywebsite.local:443/search?client=firefox-b-1-d&q=reddit&%3Cscript%3Ealert%281%29%3B%3C%2Fscript%3E")
 
 print("full url\n\t", u.geturl())
 print("filename\n\t", os.path.basename(u.path))
 print("result tuple\n\t", u)
 print("scheme\n\t", u.scheme)
+# the full portion between the schema and path
 print("netloc\n\t", u.netloc)
+# just the domain, excluding username, password, and port
+print("hostname\n\t", u.hostname)
 print("path\n\t", u.path)
 print("params\n\t", u.params)
 print("query\n\t", u.query)
@@ -87,3 +101,30 @@ print("fragment\n\t", u.fragment)
 print("username\n\t", u.username)
 print("password\n\t", u.password)
 print("port\n\t", u.port)
+
+
+#=============
+# function to decode punycode
+#=============
+
+
+def convert_punycode(ioc):
+	"""International punycode checks; searches each character individually and decodes the IOC if needed."""
+	is_it_punycode = False
+	for char in ioc:
+		#if not re.search("[A-Za-z0-9.-]", char):
+		if ord(char) > 127:
+			is_it_punycode = True
+	if is_it_punycode:
+		try:
+			ioc = ioc.encode("idna").decode().strip()
+		except Exception as e:
+			#print(str(e))
+			pass
+	return ioc
+
+
+# example domain, just a copy paste of text from NHK with .com appended
+intl_domain = "NHKやさしいことばニュース.com"
+puny_domain = convert_punycode(intl_domain)
+print(puny_domain) # xn--nhk-u63b1cko2lyc6jrwxgom6k.com
