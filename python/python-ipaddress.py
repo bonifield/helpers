171b276bd5e4ea9a22b9b86de622de554e372b0c
bonifield <bonifield.tools@gmail.com>
2026-09-25 17:11:28 -0500

    used f-string for better readability in python-ipaddress.py

--- python/python-ipaddress.py
+++ python/python-ipaddress.py
@@ -10,19 +10,19 @@ ip = ipaddress.ip_address("192.168.1.1")
 ip2 = ipaddress.ip_address("10.10.10.10")
 
 
-print("compressed", ip.compressed)
-print("exploded", ip.exploded)
-print("is_global", ip.is_global)
-print("is_link_local", ip.is_link_local)
-print("is_loopback", ip.is_loopback)
-print("is_multicast", ip.is_multicast)
-print("is_private", ip.is_private)
-print("is_reserved", ip.is_reserved)
-print("is_unspecified", ip.is_unspecified)
-print("max_prefixlen", ip.max_prefixlen)
-print("packed", ip.packed)
-print("reverse_pointer", ip.reverse_pointer)
-print("version", ip.version)
+print(f"{ip.compressed=}")
+print(f"{ip.exploded=}")
+print(f"{ip.is_global=}")
+print(f"{ip.is_link_local=}")
+print(f"{ip.is_loopback=}")
+print(f"{ip.is_multicast=}")
+print(f"{ip.is_private=}")
+print(f"{ip.is_reserved=}")
+print(f"{ip.is_unspecified=}")
+print(f"{ip.max_prefixlen=}")
+print(f"{ip.packed=}")
+print(f"{ip.reverse_pointer=}")
+print(f"{ip.version=}")
 print()
 
 
@@ -30,25 +30,25 @@ net = ipaddress.IPv4Network("10.0.0.0/8")
 # also has above is_* attributes
 
 
-print("network_address", net.network_address)
-print("broadcast_address", net.broadcast_address)
-print("hostmask", net.hostmask)
-print("netmask", net.netmask)
-print("with_prefixlen", net.with_prefixlen)
-print("compressed", net.compressed)
-print("exploded", net.exploded)
-print("with_netmask", net.with_netmask)
-print("with_hostmask", net.with_hostmask)
-print("num_addresses", net.num_addresses)
-print("prefixlen", net.prefixlen)
-print("hosts() generator", net.hosts())
+print(f"{net.network_address=}")
+print(f"{net.broadcast_address=}")
+print(f"{net.hostmask=}")
+print(f"{net.netmask=}")
+print(f"{net.with_prefixlen=}")
+print(f"{net.compressed=}")
+print(f"{net.exploded=}")
+print(f"{net.with_netmask=}")
+print(f"{net.with_hostmask=}")
+print(f"{net.num_addresses=}")
+print(f"{net.prefixlen=}")
+print(f"{net.hosts()=}")
 print()
 
 
 net2 = ipaddress.IPv4Network("10.10.0.0/16")
 
 
-print(f"net: {net}, net2: {net2}")
+print(f"{net=}, {net2=}")
 print("net overlaps with net2", net.overlaps(net2))
 print("net2 overlaps with net", net2.overlaps(net))
 print("net is a subnet of net2", net.subnet_of(net2))
@@ -67,34 +67,34 @@ print(check_cidr(ip2, net2))
 
 
 '''
-compressed 192.168.1.1
-exploded 192.168.1.1
-is_global False
-is_link_local False
-is_loopback False
-is_multicast False
-is_private True
-is_reserved False
-is_unspecified False
-max_prefixlen 32
-packed b'\xc0\xa8\x01\x01'
-reverse_pointer 1.1.168.192.in-addr.arpa
-version 4
+ip.compressed='192.168.1.1'
+ip.exploded='192.168.1.1'
+ip.is_global=False
+ip.is_link_local=False
+ip.is_loopback=False
+ip.is_multicast=False
+ip.is_private=True
+ip.is_reserved=False
+ip.is_unspecified=False
+ip.max_prefixlen=32
+ip.packed=b'\xc0\xa8\x01\x01'
+ip.reverse_pointer='1.1.168.192.in-addr.arpa'
+ip.version=4
 
-network_address 10.0.0.0
-broadcast_address 10.255.255.255
-hostmask 0.255.255.255
-netmask 255.0.0.0
-with_prefixlen 10.0.0.0/8
-compressed 10.0.0.0/8
-exploded 10.0.0.0/8
-with_netmask 10.0.0.0/255.0.0.0
-with_hostmask 10.0.0.0/0.255.255.255
-num_addresses 16777216
-prefixlen 8
-hosts() generator <generator object _BaseNetwork.hosts at 0x71fea9cb2c70>
+net.network_address=IPv4Address('10.0.0.0')
+net.broadcast_address=IPv4Address('10.255.255.255')
+net.hostmask=IPv4Address('0.255.255.255')
+net.netmask=IPv4Address('255.0.0.0')
+net.with_prefixlen='10.0.0.0/8'
+net.compressed='10.0.0.0/8'
+net.exploded='10.0.0.0/8'
+net.with_netmask='10.0.0.0/255.0.0.0'
+net.with_hostmask='10.0.0.0/0.255.255.255'
+net.num_addresses=16777216
+net.prefixlen=8
+net.hosts()=<generator object _BaseNetwork.hosts at 0x7e24c1189700>
 
-net: 10.0.0.0/8, net2: 10.10.0.0/16
+net=IPv4Network('10.0.0.0/8'), net2=IPv4Network('10.10.0.0/16')
 net overlaps with net2 True
 net2 overlaps with net True
 net is a subnet of net2 False
